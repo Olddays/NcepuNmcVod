bcd6f4a672814d0c20f17fdbfb67b497dfb572c3
Olddays <lls6145219@gmail.com>
2015-08-30 13:36:12 +0800

    Cache

---
+++ app/src/main/java/edu/nmc/bd/ncepu/nmcvod/common/analysis/cache.java
@@ -0,0 +1,7 @@
+package edu.nmc.bd.ncepu.nmcvod.common.analysis;
+
+/**
+ * Created by xuxuejun on 15-8-30.
+ */
+public class cache {
+}
---
+++ app/src/main/java/edu/nmc/bd/ncepu/nmcvod/optimizer/httpmgr/cache.java
@@ -0,0 +1,7 @@
+package edu.nmc.bd.ncepu.nmcvod.optimizer.httpmgr;
+
+/**
+ * Created by xuxuejun on 15-8-30.
+ */
+public class cache {
+}
